713b7c9501f0147ebe660f49cfc558a61886cc7b
Adza Zarif Nur iskandar <adzazarf@gmail.com>
2024-11-23 01:03:24 +0700

    fix(api): change to device code

--- app/Http/Controllers/Api/RecordController.php
+++ app/Http/Controllers/Api/RecordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Device;
 use App\Models\Record;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
@@ -11,23 +12,26 @@ class RecordController extends Controller
 {
     public function apiCreate(Request $request)
     {
+        $device = Device::where('code', $request->device_id)->first();
+        if(!$device) return response()->json(['status' => 404], 404);
+
         $temperature = number_format($request->temperature, 2); 
         $humidity = number_format($request->humidity, 2);
         Record::create([
-            'device_id' => $request->device_id,
+            'device_id' => $device->id,
             'type' => "temperature",
             'value' => $temperature
         ]);
         Record::create([
-            'device_id' => $request->device_id,
+            'device_id' => $device->id,
             'type' => "humidity",
             'value' => $humidity,
         ]);
 
-        Http::post('localhost:3000/send', [
-            'temp' => $temperature,
-            'humi' => $humidity,
-        ]);
+        // Http::post('localhost:3000/send', [
+        //     'temp' => $temperature,
+        //     'humi' => $humidity,
+        // ]);
 
         return response()->json(['status' => 200]);
     }
