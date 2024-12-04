ddde8e4030aa8482ee4339dd7019035536dcf2bd
Adza Zarif Nur iskandar <adzazarf@gmail.com>
2024-12-04 23:08:27 +0700

    add: api stats

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\RecordController;
+use App\Http\Controllers\Api\StatsController;
 use App\Http\Controllers\Api\TemperatureController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,3 +19,6 @@ Route::controller(AuthController::class)
 
 Route::post('/create-record',[RecordController::class,'apiCreate']);
 Route::get('/temp-threshold/{device_id}',[TemperatureController::class,'tempThreshold']);
+
+Route::get('/stats/{incubator_id}', [StatsController::class, 'stats']);
+Route::get('/stats/{incubator_id}/detail', [StatsController::class, 'statsDetail']);
